52863e10bfc269aa0fcf93014ef5c6d3b128c3e4
Lugas Septiawan <lugassawan@gmail.com>
2018-07-09 16:31:43 +0700

    Setup mongodb

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,6 +1,6 @@
 const expect = require('expect');
 const request = require('supertest');
-const { ObjectID } = require('mongoDB');
+const { ObjectID } = require('mongodb');
 
 const { app } = require('./../server');
 const { Todo } = require('./../models/todo');
