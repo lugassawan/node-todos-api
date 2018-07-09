c701e7ee95ab7239dc8b874f839b988bcbaf63cc
Lugas Septiawan <lugassawan@gmail.com>
2018-07-09 16:50:00 +0700

    Fix a connection to mLab

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -4,6 +4,14 @@ mongoose.Promise = global.Promise;
 //Using add on in heroku
 //mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp', { useNewUrlParser: true });
 //Using mLab directly
-mongoose.connect('mongodb://<irmajpegundan>:<irmaj12345>@ds131711.mlab.com:31711/todoapp' || 'mongodb://localhost:27017/TodoApp', { useNewUrlParser: true });
+const MONGO_URL = 'mongodb://node-api:api12345@ds131711.mlab.com:31711/todoapp';
+//mongoose.connect(MONGO_URL || 'mongodb://localhost:27017/TodoApp', { useNewUrlParser: true });
+mongoose.connect(MONGO_URL || 'mongodb://localhost:27017/TodoApp', { useNewUrlParser: true }, (err) => {
+    if (err) {
+        return console.log(err);
+    }
+
+    return console.log('Connected to mLab directly');
+});
 
 module.exports = { mongoose };
