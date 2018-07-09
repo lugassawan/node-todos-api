0ab9cce8272790f1cf4f2b5c5dd4c69c5592a519
Lugas Septiawan <lugassawan@gmail.com>
2018-07-09 16:28:40 +0700

    Setup app for heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,9 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp', { useNewUrlParser: true });
+//Using add on in heroku
+//mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp', { useNewUrlParser: true });
+//Using mLab directly
+mongoose.connect('mongodb://<irmajpegundan>:<irmaj12345>@ds131711.mlab.com:31711/todoapp' || 'mongodb://localhost:27017/TodoApp', { useNewUrlParser: true });
 
 module.exports = { mongoose };
