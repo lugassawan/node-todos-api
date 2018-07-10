09f31bc850661f8888dcb6078fbf72062ad7bf84
Lugas Septiawan <lugassawan@gmail.com>
2018-07-10 14:42:37 +0700

    Testing mLab connection

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -5,13 +5,13 @@ mongoose.Promise = global.Promise;
 //mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp', { useNewUrlParser: true });
 //Using mLab directly
 const MONGO_URL = 'mongodb://node-api:api12345@ds131711.mlab.com:31711/todoapp';
-mongoose.connect('mongodb://localhost:27017/TodoApp', { useNewUrlParser: true });
-// mongoose.connect(MONGO_URL || 'mongodb://localhost:27017/TodoApp', { useNewUrlParser: true }, (err) => {
-//     if (err) {
-//         return console.log(err);
-//     }
+//mongoose.connect('mongodb://localhost:27017/TodoApp', { useNewUrlParser: true });
+mongoose.connect(MONGO_URL || 'mongodb://localhost:27017/TodoApp', { useNewUrlParser: true }, (err) => {
+    if (err) {
+        return console.log(err);
+    }
 
-//     return console.log('Connected to mLab directly');
-// });
+    return console.log('Connected to mLab directly');
+});
 
 module.exports = { mongoose };
