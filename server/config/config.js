c826f75eefc6b53ca3dd54a2653252f2eb1eba60
Lugas Septiawan <lugassawan@gmail.com>
2018-07-19 23:31:28 +0700

    Add tests for GET /users/me and POST /users

--- server/config/config.js
+++ server/config/config.js
@@ -5,7 +5,7 @@ if (env === 'development') {
     process.env.MONGO_URI = 'mongodb://localhost:27017/TodoApp';
 } else if (env === 'test') {
     process.env.PORT = 3000;
-    process.env.MONGO_URI = 'mongodb://localhost:27017/TodoAppTest';
+    process.env.MONGO_URI = 'mongodb://127.0.0.1:27017/TodoAppTest';
 } else {
     process.env.MONGO_URI = 'mongodb://node-api:api12345@ds131711.mlab.com:31711/todoapp';
 }
