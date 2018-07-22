410188eb0434ca21bc59ea458d738505d7ea9f08
Lugas Septiawan <lugassawan@gmail.com>
2018-07-22 14:23:16 +0700

    Update config to use json file

--- server/config/config.js
+++ server/config/config.js
@@ -1,11 +1,12 @@
 var env = process.env.NODE_ENV || 'development';
 
-if (env === 'development') {
-    process.env.PORT = 3000;
-    process.env.MONGO_URI = 'mongodb://localhost:27017/TodoApp';
-} else if (env === 'test') {
-    process.env.PORT = 3000;
-    process.env.MONGO_URI = 'mongodb://127.0.0.1:27017/TodoAppTest';
-} else {
-    process.env.MONGO_URI = 'mongodb://node-api:api12345@ds131711.mlab.com:31711/todoapp';
+if (env === 'development' || env === 'test') {
+    var config = require('./config.json');
+    var envConfig = config[env];
+
+    Object.keys(envConfig).forEach((key) => {
+        process.env[key] = envConfig[key];
+    });
 }
+
+//process.env.MONGODB_URI = 'mongodb://node-api:api12345@ds131711.mlab.com:31711/todoapp';
